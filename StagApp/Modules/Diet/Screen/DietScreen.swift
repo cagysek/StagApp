fcfaa5192ec4c785e4f54d7615b7ba148cd75bb4
cagy <ml.cagy@seznam.cz>
2021-11-24 21:08:12 +0100

    More screen design

---
+++ StagApp/Modules/Diet/Screen/DietScreen.swift
@@ -0,0 +1,24 @@
+//
+//  DietScreen.swift.swift
+//  StagApp
+//
+//  Created by Jan Čarnogurský on 24.11.2021.
+//
+
+import SwiftUI
+
+struct DietScreen: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .navigationTitle("Jídelníček")
+        
+    }
+}
+
+struct DietScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DietScreen()
+    }
+}
--- StagApp/Modules/More/Screen/MoreScreen.swift
+++ StagApp/Modules/More/Screen/MoreScreen.swift
@@ -9,7 +9,69 @@ import SwiftUI
 
 struct MoreScreen: View {
     var body: some View {
-        Text("More screen")
+        NavigationView {
+            ZStack {
+                Color
+                    .defaultBackground
+                    .ignoresSafeArea()
+                
+                VStack {
+                    HStack(alignment:.bottom) {
+                        Text("Další")
+                            .font(.system(size: 32, weight: .bold, design: .rounded))
+                        Spacer()
+                    }
+                    .padding()
+                    
+                    
+                    List {
+                        Section(header: Text("ZČU").font(.system(size: 14, weight: .medium, design: .rounded))) {
+                            NavigationLink {
+                                DietScreen()
+                            } label: {
+                                Text("Jídelníček")
+                            }
+                            
+                            NavigationLink {
+                                DietScreen()
+                            } label: {
+                                Text("Webmail")
+                            }
+                            
+                            
+                        }
+
+                        Section() {
+                            NavigationLink {
+                                DietScreen()
+                            } label: {
+                                Text("Učebny")
+                            }
+                            
+                            NavigationLink {
+                                DietScreen()
+                            } label: {
+                                Text("Nastavení")
+                            }
+                            
+                            
+                        }
+                        
+                        
+                        Text("Odhlásit se")
+                    }
+                    .listStyle(.grouped)
+                    .listSectionSeparator(.hidden, edges: .all)
+                    .padding(.top, -20)
+                    
+                    
+                    
+                    Spacer()
+                }
+            }
+            .navigationBarHidden(true)
+        }
+        
     }
 }
 
