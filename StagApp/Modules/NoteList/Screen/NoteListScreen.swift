57ee54758969e85ac0d6160b6534a49540d0d143
cagy <ml.cagy@seznam.cz>
2022-04-06 14:21:00 +0200

    empty notes

--- StagApp/Modules/NoteList/Screen/NoteListScreen.swift
+++ StagApp/Modules/NoteList/Screen/NoteListScreen.swift
@@ -17,36 +17,47 @@ struct NoteListScreen: View {
     
     var body: some View {
         
-        ZStack {
+        ZStack(alignment: .top) {
             Color.defaultBackground
                 .ignoresSafeArea()
             
             VStack {
-                List {
-                    ForEach(self.vm.notes, id: \.self) { note in
-                        
-                        VStack(alignment: .leading) {
-                            Text(note.title ?? "")
-                                .font(.system(size: 16, weight: .bold, design: .rounded))
-                            Text(note.descriptionText ?? "")
-                                .lineLimit(3)
-                                .font(.system(size: 14, weight: .medium, design: .rounded))
-                                
+                if (self.vm.notes.isEmpty) {
+                    InformationBubble {
+                        VStack {
+                            Text("overview.no-notes")
+                                .font(.system(size: 16, weight: .medium, design: .rounded))
                         }
-                        .swipeActions {
-                            Button(role: .destructive) {
-                                _ = self.vm.deleteNote(note: note)
-                                
-                                self.vm.loadNotes()
-                            } label: {
-                                Label("note.delete", systemImage: "trash.fill")
+                    }
+                    .frame(height: 30)
+                    .padding()
+                } else {
+                    List {
+                        ForEach(self.vm.notes, id: \.self) { note in
+                            
+                            VStack(alignment: .leading) {
+                                Text(note.title ?? "")
+                                    .font(.system(size: 16, weight: .bold, design: .rounded))
+                                Text(note.descriptionText ?? "")
+                                    .lineLimit(3)
+                                    .font(.system(size: 14, weight: .medium, design: .rounded))
+                                    
                             }
+                            .swipeActions {
+                                Button(role: .destructive) {
+                                    _ = self.vm.deleteNote(note: note)
+                                    
+                                    self.vm.loadNotes()
+                                } label: {
+                                    Label("note.delete", systemImage: "trash.fill")
+                                }
+                            }
+                            .onTapGesture {
+                                self.selectedNote = note
+                                self.showNotesAddSheet.toggle()
+                            }
+                            
                         }
-                        .onTapGesture {
-                            self.selectedNote = note
-                            self.showNotesAddSheet.toggle()
-                        }
-                        
                     }
                 }
             }
